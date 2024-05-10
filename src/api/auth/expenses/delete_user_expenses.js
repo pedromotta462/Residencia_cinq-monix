a92c4c38183c341b20861b35161e668096c5bfb5
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-10 15:47:05 -0300

    (Waiting for approval)Added delete_user_expenses.js

---
+++ src/api/auth/expenses/delete_user_expenses.js
@@ -0,0 +1,25 @@
+import { supabase } from '../../../init';
+
+export const delete_user_expense = async (req, res) => {
+  try {
+    const expenseId = req.body.id;
+
+    if (!expenseId) {
+      res.status(400).json({ error: 'O id da despesa é obrigatório' });
+    }
+
+    const { error } = await supabase
+      .from('expenses')
+      .delete()
+      .eq('id', expenseId);
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).send("Despesa excluída com sucesso");
+  } catch (error) {
+    console.error('Erro ao excluir despesa:', error);
+    res.status(500).json({ error: 'Erro ao excluir despesa' });
+  }
+};
