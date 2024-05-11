2264eea46af4145615bede6df2975a47ac3e8104
devid <111234665+devid7734@users.noreply.github.com>
2024-05-11 20:49:39 +0000

    put e delete de investments

--- src/api/auth/investments/delete_user_investments.js
+++ src/api/auth/investments/delete_user_investments.js
@@ -1,3 +1,25 @@
-// Segue lógica do Delete da tabela Investments
+
 
 import { supabase } from "../../../init";
+
+export const delete_user_investments = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    if (!id) {
+      res.status(400).json({ error: "O id do investimento é obrigatório" });
+    }
+
+    const { error } = await supabase
+      .from("investments")
+      .delete()
+      .eq("id", id);
+
+    if (error) throw error;
+
+    res.status(200).send("investimento excluído com sucesso");
+  } catch (error) {
+    console.error("Erro ao excluir investimento:", error);
+    res.status(500).json({ error: "Erro ao excluir investimento" });
+  }
+};
--- src/api/auth/investments/put_user_investments.js
+++ src/api/auth/investments/put_user_investments.js
@@ -1,3 +1,34 @@
-// Segue lógica do Put da tabela Investments
+
 
 import { supabase } from "../../../init";
+export const put_user_investments = async (req, res) => {
+    try {
+        const id = req.params.id;
+        const new_investments = {
+            date: req.body.date,
+            value: req.body.value,
+            planner: req.body.planner,
+            description: req.body.description,
+            frequency: req.body.frequency,
+            origin: req.body.origin
+            
+        }
+
+        if (!req.body.date || !req.body.value || !req.body.planner || !req.body.description || !req.body.frequency || !req.body.origin ) {
+            res
+            .status(400)
+            .json({error: 'Todos os campos são obrigatórios'});
+        }
+        
+        const { error } = await supabase
+            .from('investments')
+            .update(new_investments)
+            .eq("id", id);
+
+        if (error) throw error;
+
+        res.status(200).send('dados atualizado com sucesso');
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao atualizar os dados'});
+    }
+};
