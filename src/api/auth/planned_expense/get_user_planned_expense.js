507b0a4455d6a40f16feb3958d2aab1a7fb4703b
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-17 12:02:55 -0300

    Adicionado método get de planned_expense

---
+++ src/api/auth/planned_expense/get_user_planned_expense.js
@@ -0,0 +1,19 @@
+import { supabase } from '../../../init';
+
+export const get_user_planned_expense = async (req, res) => {
+    try{
+        const { data, error } = await supabase
+        .from('planned_expenses')
+        .select(`
+            id,
+            user_id,
+            mounth,
+            year
+        `)
+        .eq('user_id', req.user.id)
+    
+        res.status(200).send(data);
+    } catch (error) {
+        res.status(500).json({ error: 'Erro ao recuperar categorias' });
+    }
+}
