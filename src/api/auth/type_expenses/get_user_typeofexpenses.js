beac9ed8ca6d2fbf72a80bf227613297584a33d9
Artur Inocêncio ferreira dos santos <90269415+Artur-Inocencio@users.noreply.github.com>
2024-05-17 17:29:18 -0300

    COMMIT GET AND PUT TYPEOFEXPENSES

---
+++ src/api/auth/type_expenses/get_user_typeofexpenses.js
@@ -0,0 +1,23 @@
+import { supabase } from '../../../init';
+
+export const get_user_expenses = async (req, res) => {
+  try {
+    const { data, error } = await supabase
+      .from('type of expenses')
+      .select(`
+        id,
+        user_id,
+         enum
+      `)
+      .eq('user_id', req.user.id);
+
+    if (error) {
+      throw error;
+    }
+
+    res.status(200).send(data);
+  } catch (error) {
+    console.error('Erro ao recuperar o tipo de despesa:', error);
+    res.status(500).json({ error: 'Erro ao recuperar o tipo de despesa' });
+  }
+};
