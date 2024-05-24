3c34a1231a871ed83d64f2d4040f2c3517b8eeee
Artur Santos <arturinocencioo@gmail.com>
2024-05-24 03:20:43 -0300

    Correção código type expense

--- src/api/auth/expenses/post_user_expenses.js
+++ src/api/auth/expenses/post_user_expenses.js
@@ -1,5 +1,6 @@
 import { supabase } from '../../../init';
 import { createTypeExpenses } from '../../../database/createTypeExpenses.js';
+import {v4 as uuidv4} from 'uuid';
 
 export const post_user_expense = async (req, res) => {
   try {
@@ -10,12 +11,11 @@ export const post_user_expense = async (req, res) => {
       || !req.body.subcategory_id
       || !req.body.member_id
       || !req.body.account_id
-      || !req.body.expense_type_id
       || !req.body.description
     ) {
       res.status(400).json({ error: 'É necessário preencher todos os campos' });
     }
-
+    let myuuid = uuidv4();
     let query = await supabase 
       .from('expenses')
       .insert({
@@ -27,16 +27,17 @@ export const post_user_expense = async (req, res) => {
         subcategory_id: req.body.subcategory_id,
         member_id: req.body.member_id,
         account_id: req.body.account_id,
-        expense_type_id: req.body.expense_type_id,
+        expense_type_id:myuuid,
         description: req.body.description
       });
 let error = query["error"]
     if (error) {
       throw error;
     }
+    console.log(myuuid)
     console.log(query)
     console.log(req.user.id)
-    let cte=await createTypeExpenses ({user_id:req.user.id})
+    let cte=await createTypeExpenses ({user_id:req.user.id,id:myuuid})
     console.log(cte)
     res.status(201).send("Despesa criada com sucesso");
   } catch (error) {
