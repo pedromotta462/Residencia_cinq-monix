f18fc98838114ddbd30b863989ca741eed682cb4
ialeAlmeida <almeidaialesantos@gmail.com>
2024-05-31 14:10:16 -0300

    adicionado: método post da tabela planned_expense_category

---
+++ src/api/auth/planned_expense_category/post_user_planned_expense_category.js
@@ -0,0 +1,28 @@
+import { supabase } from '../../../init';
+
+export const post_user_planned_expense_category = async (req, res) => {
+    try {
+        if (!req.body.value
+        ) {
+            res.status(400).json({ error: 'É necessário preencher todos os campos' });
+        }
+
+        const { error } = await supabase
+        .from('planned_expense_category')
+        .insert({
+            planned_expense_id: req.user.planned_expense_id,
+            category_id: req.body.category_id,
+            value: req.body.value
+        });
+
+        if (error) {
+            throw error;
+        }
+
+        res.status(201).send('Categoria de planejamento de despesa criada com sucesso');
+    } catch (error) {
+        console.log(error.message)
+        res.status(500).json({ error: 'Erro ao criar categoria de planejamento de despesa' });
+    }
+
+}
