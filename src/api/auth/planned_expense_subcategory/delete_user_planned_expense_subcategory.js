ab7b12846bb96fae6a9985f09a4fbd15cd505e89
Luís Silvestre <64700304+louixpng@users.noreply.github.com>
2024-05-31 16:03:47 +0000

    feat: add planned_expense_subcategory delete method

---
+++ src/api/auth/planned_expense_subcategory/delete_user_planned_expense_subcategory.js
@@ -0,0 +1,25 @@
+import { supabase } from '../../../init';
+
+export const delete_user_planned_expense_subcategory = async (req, res) => {
+    try {
+        const planned_expense_subcategory_id = req.params.id;
+
+        if (!planned_expense_subcategory_id) {
+            res.status(400).json({ error: 'O id da subcategoria de despesa planejada é obrigatório' });
+        }
+
+        const { error } = await supabase
+            .from('planned_expense_subcategory')
+            .delete()
+            .eq('id', planned_expense_subcategory_id);
+
+        if (error) {
+            throw error;
+        }
+        
+        res.status(200).send("Categoria de entrada planejada excluído com sucesso");
+        } catch (error) {
+            console.error('Erro ao excluir subcategoria de despesa planejada:', error);
+            res.status(500).json({ error: 'Erro ao excluir subcategoria de despesa planejada' });
+        }
+    };
