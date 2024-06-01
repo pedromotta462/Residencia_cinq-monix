b25b5b58b906ad00087c4fd46bcf1b5dce652f5b
Pedro Tavares <pedrotavaresbol1@gmail.com>
2024-05-31 21:41:55 -0300

    feat: login google

--- src/api/post_login.js
+++ src/api/post_login.js
@@ -1,11 +1,37 @@
-import { authLogin } from './../auth/authLogin';
+import { authLogin, googleLoginUrl, handleGoogleCallback } from './../auth/authLogin.js';
 
 export const post_login = async (req, res) => {
-  let { email, password } = req.body;
-  try {
-    let jwt = await authLogin(email, password);
-    res.status(200).send(jwt);
-  } catch (error) {
-    res.status(400).send("Falha no login")
+  const { email, password } = req.body;
+
+  if (email && password) {
+    // Email/password login
+    try {
+      const jwt = await authLogin(email, password);
+      res.status(200).json({ token: jwt });
+    } catch (error) {
+      res.status(400).send("Falha no login");
+    }
+  } else {
+    res.status(400).send("Email e senha são necessários para login");
   }
-}
+};
+
+export const get_google_login = async (req, res) => {
+  try {
+    const url = await googleLoginUrl();
+    res.status(200).json({ url });
+  } catch (error) {
+    res.status(400).send("Falha no login com Google");
+  }
+};
+
+export const post_google_callback = async (req, res) => {
+  const { token } = req.body;
+
+  try {
+    const user = await handleGoogleCallback(token);
+    res.status(200).json(user);
+  } catch (error) {
+    res.status(400).send("Falha no login com Google");
+  }
+};
